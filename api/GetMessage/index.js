0715ab454bb4df1622893fa18c2f6a10cba0de07
Sudhi Ramamurthy <suramam@microsoft.com>
2020-05-25 15:19:44 -0700

    API addition

---
+++ api/GetMessage/index.js
@@ -0,0 +1,27 @@
+module.exports = async function (context, req) {
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: { text: "Hello from the API" },
+        headers: { 
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Pref-Agent': 'custom'
+          }
+    };
+};
+// module.exports = async function (context, req) {
+//     context.log('JavaScript HTTP trigger function processed a request.');
+
+//     if (req.query.name || (req.body && req.body.name)) {
+//         context.res = {
+//             // status: 200, /* Defaults to 200 */
+//             body: "Hello " + (req.query.name || req.body.name)
+//         };
+//     }
+//     else {
+//         context.res = {
+//             status: 400,
+//             body: "Please pass a name on the query string or in the request body"
+//         };
+//     }
+// };
